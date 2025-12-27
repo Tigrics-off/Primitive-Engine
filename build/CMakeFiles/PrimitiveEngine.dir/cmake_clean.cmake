f02eb01369c6f4fb00c23b5c9a222c68098063dd
Tigran <tarasdarovtigran@gmail.com>
2025-12-27 16:43:24 +0300

    skeleton

---
+++ build/CMakeFiles/PrimitiveEngine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrimitiveEngine.dir/src/main.cpp.obj"
+  "CMakeFiles/PrimitiveEngine.dir/src/main.cpp.obj.d"
+  "PrimitiveEngine.exe"
+  "PrimitiveEngine.exe.manifest"
+  "PrimitiveEngine.pdb"
+  "libPrimitiveEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PrimitiveEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
